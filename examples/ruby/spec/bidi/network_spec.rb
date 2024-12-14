f720c45e1d30a1effbdde0d2bc5f3deccbd205c7
Augustin Pequeno <augustin.gottlieb@merkle.com>
2024-12-14 16:22:14 +0100

    Add skips until new Selenium release

--- examples/ruby/spec/bidi/network_spec.rb
+++ examples/ruby/spec/bidi/network_spec.rb
@@ -4,32 +4,32 @@ RSpec.describe 'Network' do
   let(:driver) { start_bidi_session }
   let(:wait) { Selenium::WebDriver::Wait.new(timeout: 2) }
 
-  it 'adds an auth handler' do
+  it 'adds an auth handler', skip: 'until Selenium release 4.28.0' do
     network = driver.network
     network.add_authentication_handler('username', 'password')
     expect(network.callbacks.count).to be 1
   end
 
-  it 'adds a request handler' do
+  it 'adds a request handler', skip: 'until Selenium release 4.28.0' do
     network = driver.network
     network.add_request_handler
     expect(network.callbacks.count).to be 1
   end
 
-  it 'adds a response handler' do
+  it 'adds a response handler', skip: 'until Selenium release 4.28.0' do
     network = driver.network
     network.add_response_handler
     expect(network.callbacks.count).to be 1
   end
 
-  it 'removes a handler' do
+  it 'removes a handler', skip: 'until Selenium release 4.28.0' do
     network = driver.network
     id = network.add_request_handler
     network.remove_handler(id)
     expect(network.callbacks.count).to be 0
   end
 
-  it 'clears all handlers' do
+  it 'clears all handlers', skip: 'until Selenium release 4.28.0' do
     network = driver.network
     network.add_request_handler
     network.add_request_handler
